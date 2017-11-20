e52768d9f23ae8fed3f05c5396a214620e5c7aa3
Alimzhan <alimzhan.amanov@gmail.com>
2017-11-20 19:31:28 +0600

    active links fixed

--- theme/index.php
+++ theme/index.php
@@ -3,9 +3,9 @@
 <?php 
 $url = $_SERVER['REQUEST_URI'];
 if ($url == '/works' || $url == '/works/') {
-	get_template_part( 'works', get_post_format() );		
+	get_template_part( 'works', get_post_format() );
 } else if ($url == '/info' || $url == '/info/') {
-	get_template_part( 'info', get_post_format() );		
+	get_template_part( 'info', get_post_format() );
 } else {
 	get_template_part( 'content', get_post_format() );
 }
