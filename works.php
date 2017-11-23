2b9b1ab2e42314a8107eea6cdd8d6c342eb92ea3
Alimzhan <alimzhan.amanov@gmail.com>
2017-11-23 15:03:29 +0600

    theme build

--- works.php
+++ works.php
@@ -38,6 +38,14 @@
             <div class="desc">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non nibh ac eros ullamcorper pulvinar. Aliquam vitae magna vitae turpis feugiat bibendum at vitae metus. Aenean ac eros odio. Pellentesque posuere, mi vitae bibendum accumsan, orci quam auctor eros, id rhoncus elit erat a nulla. Donec ac tortor vitae odio sodales convallis vel in sem. Nunc luctus pharetra lectus, a feugiat mauris vehicula ut. Ut lobortis commodo arcu ac sodales.
             </div>
+            <div class="to-work">
+              <a href="https://itunes.apple.com/kz/app/oos-form/id1221829411?mt=8">
+                <img src="img/ios-badge.svg" alt="ios" />
+              </a>
+              <a class="android" href="https://play.google.com/store/apps/details?id=com.solf.jtiapp">
+                <img src="img/android-badge.svg" alt="android" />
+              </a>
+            </div>
           </div>
           <div class="right-column col-sm-6 col-xs-5">
             <div class="photo">
@@ -64,6 +72,13 @@
             </div>
             <div class="desc">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non nibh ac eros ullamcorper pulvinar. Aliquam vitae magna vitae turpis feugiat bibendum at vitae metus. Aenean ac eros odio. Pellentesque posuere, mi vitae bibendum accumsan, orci quam auctor eros, id rhoncus elit erat a nulla.
+            </div>
+            <div class="to-work">
+                <div class="contact-button">
+                  <a href="https://buro-interiors.com">
+                    <button type="button">Open website</button>
+                  </a>
+                </div>
             </div>
           </div>
         </div>
